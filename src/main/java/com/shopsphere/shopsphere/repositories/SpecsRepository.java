5be12b926f784ac09efdced3cd479346f831a5e4
Endry LUNDY ANDRES <clansdoli@gmail.com>
2024-12-27 14:25:36 +0100

    added repo

---
+++ src/main/java/com/shopsphere/shopsphere/repositories/SpecsRepository.java
@@ -0,0 +1,7 @@
+package com.shopsphere.shopsphere.repositories;
+
+import com.shopsphere.shopsphere.models.Specs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecsRepository extends JpaRepository<Specs,Integer> {
+}
