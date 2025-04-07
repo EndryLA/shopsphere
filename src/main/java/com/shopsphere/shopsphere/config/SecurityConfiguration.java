3be7b6ad594a22f719817d66780fb43cd9ce55d4
Endry LUNDY ANDRES <clansdoli@gmail.com>
2025-04-08 00:09:08 +0200

    added security config

--- src/main/java/com/shopsphere/shopsphere/config/SecurityConfiguration.java
+++ src/main/java/com/shopsphere/shopsphere/config/SecurityConfiguration.java
@@ -1,15 +1,33 @@
 package com.shopsphere.shopsphere.config;
 
+import com.shopsphere.shopsphere.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Value("${spring.security.saltRounds}")
+    private int saltRounds;
+
+    private CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
 
     @Bean
@@ -22,6 +40,25 @@ public class SecurityConfiguration {
                 );
 
          return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig) throws Exception {
+        return cofig.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder(saltRounds));
+        authProvider.setUserDetailsService(customUserDetailsService);
+
+        return authProvider;
     }
 
 }
