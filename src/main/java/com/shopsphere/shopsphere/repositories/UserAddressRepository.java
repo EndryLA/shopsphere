b38f9e02179a32516948cff96fe4d4decea2f01f
Endry LUNDY ANDRES <clansdoli@gmail.com>
2025-04-18 04:16:00 +0200

    deleted userAdress related classes

--- src/main/java/com/shopsphere/shopsphere/repositories/UserAddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopsphere.shopsphere.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress, Integer> {
-
-}
